a2aae4fa395640c46ea8eba0116712df37f27458
mclinica_espejo <espejo@mclinica.com>
2019-06-22 16:12:05 +0800

    code optimisation

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,3 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+let onDeviceReady = () => {
+    platformBrowserDynamic().bootstrapModule(AppModule);
+  };
+  document.addEventListener('deviceready', onDeviceReady, false);
